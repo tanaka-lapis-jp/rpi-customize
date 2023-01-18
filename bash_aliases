ae942fd37a654894fd82089220a6db8da0dfebc2
Yoshiyuki Tanaka <tanaka802@lapis-tech.com>
2023-01-18 14:13:26 +0900

    bash_aliasesの追加

---
+++ bash_aliases
@@ -0,0 +1,4 @@
+alias ll='ls -laFG'
+alias l=ll
+alias '..'='cd ..'
+alias '...'='cd ..; cd ..'
--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cp vimrc ~/.vimrc
+if [ ! -f ~/.bash_aliases ]; then
+  cp bash_aliases ~/.bash_aliases
+  source ~/.bash_aliases
+fi
+if [ ! -f ~/.vimrc ]; then
+  cp vimrc ~/.vimrc
+fi
 cd ~
 mkdir -p .vim/colors
 cd .vim
